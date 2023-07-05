02dc16a41651a7af5eec69b388f9c80cb92654fe
CarlMutungah <138567666+CarlMutungah@users.noreply.github.com>
2023-07-05 15:26:46 +0300

    calc

---
+++ calc.py
@@ -0,0 +1,13 @@
+import pdb
+
+#function to add, multiply
+def add(a,b):
+  sum = a+b
+  return sum
+
+def multiply(a,b):
+  product = a*b
+  return product
+
+a= int(input("give number a: "))
+b= int(input("give number b: "))
